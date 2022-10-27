b95b57a73c67747e64e0ed7119335083c54121ef
Owen <ojr4316@rit.edu>
2022-10-27 16:11:10 -0400

    init

---
+++ Components/ImageCard.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import moment from "moment";
+import styles from "../styles/ImageCard.module.css";
+
+type Props = {
+  image: string;
+  date: Date;
+  confidence: number;
+};
+
+export function ImageCard(props: Props) {
+  return (
+    <div className={styles.container}>
+      <img src={props.image} alt="ImageCard" className={styles.image} />
+      <div style={{ display: "flex", padding: "4px", justifyContent: "space-between" }}>
+        <p className={styles.confidence}>{props.confidence}%</p>
+        <p className={styles.date}>{moment(props.date).fromNow()}</p>
+      </div>
+    </div>
+  );
+}
